6fd3df1936e3225f53d7ebc6d2ea6ebd62f41618
Rohit <90600620+rohit2k3@users.noreply.github.com>
2021-10-14 01:48:31 +0530

    Update and rename averagesum.java to AverageSum.java

--- Programs/Java/AverageSum.java
+++ Programs/Java/AverageSum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class averagesum {
+public class AverageSum {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter n numbers to get Average ");
